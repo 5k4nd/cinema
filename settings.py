e8f3af6f71db3c12c8c066978c2d3d7b4f874873
Bapt Abl <skand.git@mailo.com>
2024-05-23 22:26:52 +0200

    Minor changes to config PATHS

--- settings.py
+++ settings.py
@@ -5,11 +5,12 @@ from pwd import getpwuid
 
 locale.setlocale(locale.LC_TIME, "fr_FR.UTF-8")
 
-TEMPLATES = Path("templates")
-OUT_PATH = Path("html")
-OUT_USER = getpwuid(getuid()).pw_name  # you can overdrive ex. with "bat"
-OUT_GROUP = OUT_USER  # ex. "www-data" or "staff" on local OSX dev
+CODE_DIR = Path(".")  # use absolute path if using systemd service
+OUT_PATH = Path("html")    # use absolute path if using systemd service
+OUT_USER = getpwuid(getuid()).pw_name # you can overdrive ex. with "foo"
+OUT_GROUP = OUT_USER # ex. "www-data" or "staff" on local OSX dev
 
-SOURCE_PATH = "cinemas.csv"
+TEMPLATES = CODE_DIR / "templates"
+SOURCE_PATH = CODE_DIR / "cinemas.csv"  # todo: rename to THEATERS_SOURCE_FILE
 MAIN_CITY = "Besançon"
 COMPRESS_PIC = True
