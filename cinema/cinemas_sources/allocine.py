f8b4ca1878bdcc2c001e61a314123d5f6685280e
Bapt Abl <skand.git@mailo.com>
2024-12-02 13:12:08 +0100

    Fix n+1 days fetching by using absolute day dates instead of relative ones

--- cinema/cinemas_sources/allocine.py
+++ cinema/cinemas_sources/allocine.py
@@ -1,9 +1,9 @@
 import subprocess
-from datetime import date, datetime
+from datetime import date, datetime, timedelta
 from hashlib import md5
 from itertools import chain
 from pathlib import Path
-from typing import Dict, List, TYPE_CHECKING, Optional
+from typing import TYPE_CHECKING, Dict, List, Optional
 from urllib.parse import quote_plus
 
 import requests
@@ -14,6 +14,7 @@ from cinema.exceptions import RemoteResourceException
 from cinema.models import FilmShow
 from cinema.settings import COMPRESS_PIC, OUT_PATH
 
+
 if TYPE_CHECKING:
     from cinema.main import Cinema
 
@@ -21,7 +22,7 @@ SERVICE_URL = "http://www.allocine.fr"
 SERVICE_TYPE = "allocine"
 
 
-def _parse_directors(movie_meta: dict) -> str:
+def _extract_directors(movie_meta: dict) -> str:
     directors_list = []
     directors = movie_meta["credits"]
     if isinstance(directors, dict):
@@ -34,7 +35,43 @@ def _parse_directors(movie_meta: dict) -> str:
             # firstname = f"{firstname[0]}." if firstname else ''
             directors_list.append(person["person"].get("lastName", ""))
 
-    return ','.join(directors_list)
+    return ",".join(directors_list)
+
+
+def _extract_release_date(movie_meta: dict) -> Optional[datetime]:
+    release_date = None
+    for release in movie_meta["releases"][::-1]:
+        # explore releases from oldest (last ones) to newest
+        if release.get("releaseDate"):
+            release_date = dateutil_parse(release["releaseDate"]["date"])
+            current_year = date.today().year
+            # we only want month and day for current year movies
+            release_date = (
+                datetime.strftime(release_date, "%d/%m") if release_date.year == current_year else release_date.year
+            )
+            break
+
+    return release_date
+
+
+def _extract_show_times(movie_showtimes: dict) -> set:
+    showtimes = set()  # set prevents duplicates
+
+    # shows are divided across dubbed, original, local, etc., so we need to merge them before anything
+    showtime_list = chain.from_iterable(movie_showtimes.values())
+
+    for showtime in showtime_list:
+        lang_tags = showtime["tags"]
+        if lang_tags and isinstance(lang_tags, list) and len(lang_tags) > 0:
+            lang = lang_tags[0].replace("Localization.Language.", "")
+        else:
+            # unknown lang tags format or content, let's assert the show is VO
+            lang = None
+        lang = "VF" if lang == "French" else "VO"
+        show_date = dateutil_parse(showtime["startsAt"])
+        showtimes.add(f"{show_date.hour}:{str(show_date.minute).zfill(2)} ({lang})")
+
+    return showtimes
 
 
 def fetch_next_week_shows(cinemas: Dict[str, List["Cinema"]]) -> Dict[str, List[List[FilmShow]]]:
@@ -50,81 +87,56 @@ def fetch_next_week_shows(cinemas: Dict[str, List["Cinema"]]) -> Dict[str, List[
         for cinema in city_cinemas:
             if cinema.type != SERVICE_TYPE:
                 continue
-            for day_from_today in range(0, 7):
-                print(f"Fetching {cinema.name} shows day today+{day_from_today}...")
-                resource = f"{SERVICE_URL}/_/showtimes/theater-{cinema.code}/d-{day_from_today}/"
+
+            coming_week_days = [date.today() + timedelta(days=i) for i in range(7)]
+            for day_idx, current_day in enumerate(coming_week_days):
+                formatted_day = current_day.strftime("%Y-%m-%d")
+                print(f"Fetching {cinema.name} shows for {formatted_day}...")
+                resource = f"{SERVICE_URL}/_/showtimes/theater-{cinema.code}/d-{formatted_day}/"
                 res = requests.get(resource)
+                try:
+                    res_json = res.json()
+                except JSONDecodeError:
+                    raise RemoteResourceException(resource, "invalid JSON response")
                 if res.status_code != 200:
-                    try:
-                        res_json = res.json()
-                    except JSONDecodeError:
-                        raise RemoteResourceException(resource, "invalid JSON response")
                     raise RemoteResourceException(resource, res_json)
 
-                for movie in res.json()["results"]:
+                for movie in res_json["results"]:
                     movie_meta = movie["movie"]
                     if not movie_meta:
                         # show times not linked to any movie... what is that!? skipping.
                         continue
 
-                    release_date = ""
-                    for release in movie_meta["releases"][::-1]:
-                        # explore releases from oldest (last ones) to newest
-                        if release.get("releaseDate"):
-                            release_date = dateutil_parse(release["releaseDate"]["date"])
-                            current_year = date.today().year
-                            # we only want month and day for current year movies
-                            release_date = (
-                                datetime.strftime(release_date, "%d/%m")
-                                if release_date.year == current_year
-                                else release_date.year
-                            )
-                            break
-
-                    showtimes = set()  # set prevents duplicates
-                    # shows are divided across dubbed, original, local, etc., so we need to merge them before anything
-                    showtime_list = chain.from_iterable(movie["showtimes"].values())
-                    for showtime in showtime_list:
-                        lang_tags = showtime["tags"]
-                        if lang_tags and isinstance(lang_tags, list) and len(lang_tags) > 0:
-                            lang = lang_tags[0].replace("Localization.Language.", "")
-                        else:
-                            # unknown lang tags format or content, let's assert the show is VO
-                            lang = None
-                        lang = "VF" if lang == "French" else "VO"
-                        show_date = dateutil_parse(showtime["startsAt"])
-                        showtimes.add(f"{show_date.hour}:{str(show_date.minute).zfill(2)} ({lang})")
-
                     movie_title = movie["movie"]["title"]
+                    release_date = _extract_release_date(movie_meta)
+                    showtimes = _extract_show_times(movie["showtimes"])
                     allocine_movie_url = f"/film/fichefilm_gen_cfilm={movie['movie']['internalId']}.html"
                     tags = [tag["name"].split("/")[0].split("-")[0].strip() for tag in movie_meta["relatedTags"]]
                     search_engines_query = quote_plus(f"{movie_title} {release_date}")
 
-                    director_names = _parse_directors(movie_meta)
                     poster_metas = movie_meta.get("poster")
                     poster_url = poster_metas.get("url", "") if poster_metas else ""
 
                     if not shows.get(city_name):
+                        # init the data structure that will store the shows
                         shows[city_name] = [[] for _ in range(7)]
+
                     film_show = FilmShow(
-                                label= movie_title + f"<br>({release_date})<br>{director_names}",
-                                cinema= cinema.name,
-                                allocine_url= SERVICE_URL + allocine_movie_url,
-                                yt_url= f"https://www.youtube.com/results?search_query=trailer+{search_engines_query}",
-                                sc_url= f"https://www.senscritique.com/search?query={search_engines_query}",
-                                rotten_tomatos_url= f"https://www.rottentomatoes.com/search?search={search_engines_query}",
-                                # langs=', '.join(movie_meta["languages"]),
-                                synopsis= movie_meta.get("synopsisFull") or "Synopsis indisponible",
-                                tags= " / ".join(tags),
-                                url=f"{SERVICE_URL}/seance/salle_gen_csalle={cinema.code}.html",
-                                poster_url= poster_url,
-                                seances= "<br>".join(
-                                    sorted(showtimes)) + f'<br><br>{movie_meta.get("runtime") or "??"}',
-                        )
-                    film_show.poster_url = download_poster(film_show)
-                    shows[city_name][day_from_today].append(
-                        film_show
+                        label=movie_title + f"<br>({release_date})<br>{_extract_directors(movie_meta)}",
+                        cinema=cinema.name,
+                        allocine_url=SERVICE_URL + allocine_movie_url,
+                        yt_url=f"https://www.youtube.com/results?search_query=trailer+{search_engines_query}",
+                        sc_url=f"https://www.senscritique.com/search?query={search_engines_query}",
+                        rotten_tomatos_url=f"https://www.rottentomatoes.com/search?search={search_engines_query}",
+                        # langs=', '.join(movie_meta["languages"]),
+                        synopsis=movie_meta.get("synopsisFull") or "Synopsis indisponible",
+                        tags=" / ".join(tags),
+                        url=f"{SERVICE_URL}/seance/salle_gen_csalle={cinema.code}.html",
+                        poster_url=poster_url,
+                        seances="<br>".join(sorted(showtimes)) + f'<br><br>{movie_meta.get("runtime") or "??"}',
                     )
+                    film_show.poster_url = download_poster(film_show)
+                    shows[city_name][day_idx].append(film_show)
 
             print("... done!")
 
@@ -161,9 +173,7 @@ def download_poster(film_show: FilmShow) -> Optional[str]:
                 f.write(pic_bytes)
 
             # resize images to vignettes
-            if COMPRESS_PIC and subprocess.call(
-                    ["convert", local_file_path, "-resize", "120x160", local_file_path]
-            ):
+            if COMPRESS_PIC and subprocess.call(["convert", local_file_path, "-resize", "120x160", local_file_path]):
                 print(f"Exception during resizing of {local_file_path}")
 
     local_path = f"../pic/{filename}"
